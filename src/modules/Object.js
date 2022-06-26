be3d0bf27c4ae4091d1bebe2d59cdb86050ca0b9
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2022-06-26 18:36:22 +0900

    add ExEllipse

--- src/modules/Object.js
+++ src/modules/Object.js
@@ -1,4 +1,4 @@
-import * as ex from "./Extension";
+import * as ex from './Extension';
 
 export class Obj {
   constructor(vector, width, height) {
@@ -34,5 +34,35 @@ export class Ball extends Obj {
 
   display() {
     ex.exCircle(this.position, this.width);
+  }
+}
+
+export class ExEllipse extends Obj {
+  constructor(vector, width, height, pointNum = 60) {
+    super(vector, width, height);
+    this.points = [];
+    this.pointNum = pointNum;
+    this.size = { width: this.width, height: this.height };
+    return this;
+  }
+
+  createPoints() {
+    for (let i = 0; i <= this.pointNum; i++) {
+      this.points.push(
+        createVector(
+          this.position.x + (this.size.width / 2) * cos((TAU / this.pointNum) * i),
+          this.position.y + (this.size.height / 2) * sin((TAU / this.pointNum) * i)
+        )
+      );
+    }
+    return this;
+  }
+
+  draw() {
+    beginShape();
+    this.points.forEach(point => ex.exCurveVertex(point));
+    ex.exCurveVertex(this.points[1]);
+    ex.exCurveVertex(this.points[2]);
+    endShape();
   }
 }
