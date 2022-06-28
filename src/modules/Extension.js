db320650306439d5fddd93940dacb1ff11da410b
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2022-06-28 21:13:18 +0900

    コメント追記

--- src/modules/Extension.js
+++ src/modules/Extension.js
@@ -1,13 +1,40 @@
+/**
+ * extension of line
+ */
 export const exLine = (vector1, vector2) => line(vector1.x, vector1.y, vector2.x, vector2.y);
 
+/**
+ * extension of text
+ */
 export const exText = (vector, str) => text(str, vector.x, vector.y);
 
+/**
+ * extension of circle
+ */
 export const exCircle = (vector, diameter) => circle(vector.x, vector.y, diameter);
 
+/**
+ * extension of triangle
+ */
+export const exTriangle = (vector1, vector2, vector3) =>
+triangle(vector1.x, vector1.y, vector2.x, vector2.y, vector3.x, vector3.y);
+
+/**
+ * extension of rect
+ */
 export const exRect = (vector, width, height) => rect(vector.x, vector.y, width, height);
 
+/**
+ * extension of point
+ */
 export const exPoint = vector => point(vector.x, vector.y);
 
+/**
+ * extension of vertex
+ */
 export const exVertex = vector => vertex(vector.x, vector.y);
 
+/**
+ * extension of curveVertex
+ */
 export const exCurveVertex = vector => curveVertex(vector.x, vector.y);
