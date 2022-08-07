50bad059c2c981911b02161390cdc43bd2499d2d
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2022-08-07 22:21:55 +0900

    distのgit監視をやめる

--- dist/sample/Object/sketch.js
+++
@@ -1,8 +0,0 @@
-const Obj = p5ex.Object;
-
-function setup() {
-  createCanvas(1000, 1000);
-  background(240);
-	const car = new Obj.Car(createVector(width / 2, height / 2)).display();
-	const ball = new Obj.Ball(createVector(width / 2 - 100, height / 2), 50).display();
-}
